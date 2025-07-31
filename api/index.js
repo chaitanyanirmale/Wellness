f05800bf9d728f0638940d0121b02bda5c4f5680
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2025-07-31 18:50:28 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
 dotenv.config();
+
+
 mongoose.connect(process.env.MONGO).then(() => {
   console.log('Connected to MongoDB');
 }).catch((err) => {
