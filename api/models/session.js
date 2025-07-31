77a135071b4e37329a626c61dabf4b17b60f6991
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2025-07-31 18:07:31 +0530

    some files changed

---
+++ api/models/session.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const sessionSchema = new mongoose.Schema({
+  id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  tags: {
+    type: [String],
+    default: []
+  },
+  content: {
+    type: mongoose.Schema.Types.Mixed, 
+    required: true
+    },
+  status: {
+    type: String,
+    enum: ['draft', 'published'],
+    default: 'draft'
+  },
+}, {
+    timestamps: true,
+});
+
+sessionSchema.pre('save', function (next) {
+  this.updated_at = Date.now();
+  next();
+});
+
+export default mongoose.model('Session', sessionSchema);
---
+++ wellness-app/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <header className='bg-slate-200 shadow-md'>
+    <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <Link>
+        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap text-slate-500'>Wellness</h1>
+        </Link>
+
+        <ul className='flex gap-4'>
+                <Link to='/'>
+                     <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+                </Link>
+                <Link to='/about'>
+                    <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+                </Link>
+                <Link to='/Profile'>
+                    
+                    <li className='text-slate-700 hover:underline'> Sign In</li>
+                    
+                </Link>                
+            </ul>
+    </div>
+    </header>
+  )
+}
