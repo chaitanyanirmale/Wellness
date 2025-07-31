c894886ef9cde6fd144623616b177bfb17bccd69
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2025-07-31 17:07:54 +0530

    installed tailwindcss and created pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Header from './components/Header'
 
 export default function App() {
   return (
-    <div className='text-3xl font-bold'>App</div>
+    <BrowserRouter>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/signin' element={<SignIn />}/>
+        <Route path='/signup' element={<SignUp />}/>
+      </Routes>
+    
+    </BrowserRouter>
   )
 }
